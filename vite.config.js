fa9c6718cb68ae6a1827112a93abfa219a70493d
Tobias Johansson <tobias@noq.nu>
2025-03-11 11:14:54 +0100

    deploy to test environment for frontend

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: mode === "github" ? "/noQ-Frontend-fork/" : "/",
   plugins: [react({ fastRefresh: false })],
   envPrefix: "NOQ",
   define: {
